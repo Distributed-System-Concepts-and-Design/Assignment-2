7eaa8cf8485554a5a65127a96ca709536ee0405a
Mohammad-Sufyan-Azam <sufyan20312@iiitd.ac.in>
2024-03-25 10:10:43 +0530

    Completed and tested raft implementation without leader election

--- server.py
+++ server.py
@@ -18,10 +18,13 @@ class RaftNode(raft_pb2_grpc.RaftNodeServicer):
         self.leader_id = -1
         self.leader_lease = 5
         self.SERVERS_INFO = node_addresses
-        
+
+        self.heartbeat_duration = random.randint(100, 200) / 100
+        self.timer = Timer(self.heartbeat_duration, self.leader)
         self.id = id
         self.threads = []
         self.timeout = 0
+        self.no_of_heartbeats = 0
         
         if self.id == 0:
             self.leader_id = self.id
@@ -36,37 +39,76 @@ class RaftNode(raft_pb2_grpc.RaftNodeServicer):
         # Checks if it is the leader or a follower
         # print("Starting server", self.id)
         if self.state == "Leader":
+            # Using multitimers
             self.leader()
         else:
             self.follower()
-    
+
 
     def leader(self):
         """
         Does the necessary actions after being declared as a leader.
         """
-        print(f"Sending heartbeats from node {self.id}!")
-        self.threads = []
-        for id, address in self.SERVERS_INFO.items():
-            if id == self.id: # Skip the leader itself
-                continue
-            self.threads.append(Thread(target=self.heartbeat, args=(id, address)))
-        
-        for t in self.threads:
-            t.start()
-            # t.join()
+        try:
+            start_time = time.time()
+            if self.state != "Leader":
+                print(f"Can not send heartbeats from a {self.state} node!")
+                return
 
-        # Restart the timer
-        # self.timeout = random.randint(150, 300) / 1000
-        # self.timer = Timer(self.timeout, self.leader)
-        # self.timer.start()
-        
+            self.heartbeat_duration = random.randint(100, 200) / 100
+            print(f"Sending heartbeats from node {self.id}!")
 
+            self.threads = []
+            for id, address in self.SERVERS_INFO.items():
+                if id == self.id: # Skip the leader itself
+                    continue
+                self.threads.append(Thread(target=self.heartbeat, args=(id, address)))
+                # self.threads.append(Timer(self.heartbeat_duration, self.heartbeat, args=(id, address)))
+            
+            for t in self.threads:
+                t.start()
+            
+            for t in self.threads:
+                t.join()
+            
+            # Check if majority of the nodes are alive
+            print("No of heartbeats:", self.no_of_heartbeats)
+            if self.no_of_heartbeats <= len(self.SERVERS_INFO) // 2:
+                self.no_of_heartbeats = 0
+                print(f"Majority of the nodes are down! Leader {self.id} will stop!")
+                self.leader_election()  # Perform leader election
+                # self.current_term += 1
+            else:
+                self.no_of_heartbeats = 0
+                # Restart the timer. For now, it is set to heartbeat_duration
+                end_time = time.time()
+                if end_time - start_time >= self.leader_lease:
+                    # Leader Lease expired. Perform leader election
+                    print("Leader Lease expired!")
+                    self.leader_election()
+                else:
+                    # Renew Leader Lease by restarting the timer.
+                    print("Renewing Leader Lease!")
+            
+            self.timer = Timer(self.heartbeat_duration, self.leader)
+            # self.timer.join()
+            self.timer.start()
+            # self.timer.cancel()
+
+        except Exception as e:
+            print(f"Leader {self.id} stopped!")
+    
+
+    def leader_election(self):
+        self.current_term += 1
+
+    
     def follower(self):
         """
         Does the necessary actions after being declared as a follower.
         """
         pass
+
 
     def heartbeat(self, id, address):
         """
@@ -85,102 +127,47 @@ class RaftNode(raft_pb2_grpc.RaftNodeServicer):
  
             # Formatting the entries
             entries = '\n'.join(self.log)
-            # logEntries can be a dictionary with id as s.no and value as the log entry
+            # logEntries can be a dictionary with id as s.no and value as the log entry. But currently it is treated as list of strings (logs).
             request = raft_pb2.AppendEntriesRequest(term=self.current_term, leaderId=self.id, 
                                                     logEntries=entries,
                                                     leaseDuration=self.leader_lease)
+            # print("Request:", request.term, request.leaderId, request.logEntries, request.leaseDuration)
             response = stub.AppendEntries(request)
+            if response.success:
+                self.no_of_heartbeats += 1
             
             return response
         except Exception as e:
             print(f"Error in sending heartbeat to node {id}!")
             return None
 
-    # def heartbeat(self, id, address):
-    #     """
-    #     Sends a heartbeat to a server.
-
-    #     :param id: The id of the server to send the heartbeat to.
-    #     :param address: The address of the server to send the heartbeat to.
-    #     """
-    #     if self.state != "Leader":
-    #         return
-
-    #     channel = grpc.insecure_channel(address)
-    #     stub = raft_pb2_grpc.ServiceStub(channel)
-
-    #     entries = []
-    #     if self.nextIndex[id] <= len(self.log):
-    #         entries = [self.log[self.nextIndex[id]-1]]
-
-    #     prev_log_term = 0
-    #     if self.nextIndex[id] > 1:
-    #         prev_log_term = self.log[self.nextIndex[id]-2]["term"]
-
-    #     message = raft_pb2.AppendTermIdMessage(term=int(self.term), id=int(self.id), prev_log_index=self.nextIndex[id]-1, 
-    #                                            prev_log_term=prev_log_term, entries=entries, leader_commit=self.commitIndex)
-
-    #     try:
-    #         response = stub.AppendEntries(message)
-    #         reciever_term = response.term
-    #         reciever_result = response.result
-    #         if reciever_term > self.term:
-    #             self.update_term(reciever_term)
-    #             self.set_timeout()
-    #             self.follower_declaration()
-    #         else:
-    #             if reciever_result:
-    #                 if len(entries) != 0:
-    #                     self.matchIndex[id] = self.nextIndex[id]
-    #                     self.nextIndex[id] += 1
-    #             else:
-    #                 self.nextIndex[id] -= 1
-    #                 self.matchIndex[id] = min(
-    #                     self.matchIndex[id], (self.nextIndex[id]-1))
-    #     except grpc.RpcError:
-    #         return
-
-
-    # def leader_action(self):
-        # """
-        # Sends heartbeats to followers.
-        # """
-        # if self.state != "Leader":
-        #     return
-
-        # self.threads = []
-        # for k, v in self.SERVERS_INFO.items():
-        #     if k == self.id:
-        #         continue
-        #     self.threads.append(Thread(target=self.heartbeat, args=(k, v)))
-        # for t in self.threads:
-        #     t.start()
-
-        # # self.restart_timer(self.timeout, self.leader_check)
-    
 
     def AppendEntries(self, request, context):
+        # print("Received heartbeat!", request.term, request.leaderId, request.logEntries, request.leaseDuration)
+
         response = raft_pb2.AppendEntriesResponse()
-        print("Received heartbeat!", request)
-        response.term = self.current_term
-        response.success = True
+        response.term = self.current_term   # Stores previous term
+        response.success = True             # Bullshit value
 
-        if request.term < self.current_term:
-            response.success = False
+        self.current_term = request.term
+        self.leader_id = request.leaderId
+        self.leaseDuration = request.leaseDuration
+
+        print(f"Received heartbeat from node {request.leaderId} with term {request.term}!")
+        print("Log entries:", request.logEntries)
+
+        # Update the log
+        logEntries = request.logEntries.split('\n')
+        # Iterate over the list from the back and find out the first entry that is also present in the log.
+        for i in range(len(logEntries)-1, -1, -1):
+            if logEntries[i] in self.log:
+                break
+        # Append the entries that are not present in the log.
+        for j in range(i+1, len(logEntries)):
+            self.log.append(logEntries[j])
+
         return response
-
-        # self.leader_id = request.leader_id
-        # self.current_term = request.term
-
-        # if request.prev_log_index >= len(self.log) or self.log[request.prev_log_index].term != request.prev_log_term:
-        #     response.success = False
-        #     return response
-
-        # self.log = self.log[:request.prev_log_index + 1] + request.entries
-        # self.commit_length = min(request.leader_commit, len(self.log) - 1)
-        # response.success = True
-        # return response
-
+ 
 
     def RequestVote(self, request, context):
         response = raft_pb2.RequestVoteResponse()
@@ -200,6 +187,25 @@ class RaftNode(raft_pb2_grpc.RaftNodeServicer):
             response.vote_granted = False
 
         return response
+    
+
+    def handle_get_request(self, key):
+        if self.state != "Leader":
+            return raft_pb2.ServeClientResponse(data="Not the leader!", leader_id=self.leader_id, success=False)
+        else:
+            log = "GET " + key + " " + str(self.current_term)
+            self.log.append(log)
+            return raft_pb2.ServeClientResponse(data=self.dict.get(key, "Key not present!"), leader_id=self.leader_id, success=True)
+
+
+    def handle_set_request(self, key, value):
+        if self.state != "Leader":
+            return raft_pb2.ServeClientResponse(data="Not the leader!", leader_id=self.leader_id, success=False)
+        else:
+            self.dict[key] = value
+            log = "SET " + key + " " + value + " " + str(self.current_term)
+            self.log.append(log)
+            return raft_pb2.ServeClientResponse(data="Value set succesfully!", leader_id=self.leader_id, success=True)
 
 
     def ServeClient(self, request, context):
@@ -210,21 +216,12 @@ class RaftNode(raft_pb2_grpc.RaftNodeServicer):
         
         elif request.request.split()[0] == "GET":
             key = request.request.split()[1]
-            if self.state != "Leader":
-                return raft_pb2.ServeClientResponse(data="Not the leader!", leader_id=self.leader_id, success=False)
-            else:
-                return raft_pb2.ServeClientResponse(data=self.dict.get(key, "Key not present!"), leader_id=self.leader_id, success=True)
+            return self.handle_get_request(key)
         
         elif request.request.split()[0] == "SET":
             key = request.request.split()[1]
             value = request.request.split()[2]
-            if self.state != "Leader":
-                return raft_pb2.ServeClientResponse(data="Not the leader!", leader_id=self.leader_id, success=False)
-            else:
-                self.dict[key] = value
-                log = request.request + " " + str(self.current_term)
-                self.log.append(log)
-                return raft_pb2.ServeClientResponse(data="Value set succesfully!", leader_id=self.leader_id, success=True)
+            return self.handle_set_request(key, value)
 
 
     def is_up_to_date(self, last_log_index, last_log_term):
